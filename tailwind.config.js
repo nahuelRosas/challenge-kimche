2ae8cd7f18a3db41dea5919237c5c5897fbc6eb7
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-05 12:34:20 -0300

    Update tailwind.config.js and package.json, remove unused CSS file, and modify App.tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+};
